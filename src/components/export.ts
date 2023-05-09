df042f14b51a2106afcd9e0c9e23a762e0c1f274
jhojocor <jhojocor@gmail.com>
2023-05-09 00:17:02 -0500

    debería funcionar pero no funciona

--- src/components/export.ts
+++ src/components/export.ts
@@ -1,2 +1,3 @@
-export {default as Card} from './Card/Card'
-export {default as Button} from './Button/Button'
+export {default as Figure} from './figure/figure';
+export {default as Card} from './Card/Card';
+export {default as Button} from './Button/Button';
--- src/screens/dashboard.ts
+++ src/screens/dashboard.ts
@@ -1,27 +1,31 @@
-import { AttributeCard } from "../components/Card/Card";
-import { getData } from "../services/getData";
+import { getData } from '../services/getData'
+import { AttributeCard } from '../components/Card/Card';
+import { ApiType } from '../types/apiType';
 import "../components/export"
 
-class DashBoard extends HTMLElement{
+class Dashboard extends HTMLElement {
+
     constructor(){
         super();
-        this.attachShadow({mode: 'open'});
+        this.attachShadow({mode:"open"});
     }
 
     async connectedCallback(){
         const data = await getData();
-        this.render(data);
+        this.render(data)
     }
 
     render(data: any){
-        if(this.shadowRoot) this.shadowRoot.innerHTML = `<h1>Chistes Api</h1>`;
+        if(this.shadowRoot) this.shadowRoot.innerHTML = '';
 
-        data.forEach((e: string) => {
+        data.forEach((a: ApiType) => {
             const card = this.ownerDocument.createElement('app-card');
-            card.setAttribute(AttributeCard.categories, e);
+            card.setAttribute(AttributeCard.name, a.name);
+            card.setAttribute(AttributeCard.imageUrl,a.imageUrl);
+            card.setAttribute(AttributeCard.btn_text, "Dar like");
             this.shadowRoot?.appendChild(card);
         });
     }
 }
 
-customElements.define('app-dashboard', DashBoard)
+customElements.define('app-dashboard',Dashboard);
